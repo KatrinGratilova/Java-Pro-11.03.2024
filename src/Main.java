84f3c5da7dc46ab05a0fa30b28a2296cae040df8
KatrinGratilova <gr.katrin.05@gmail.com>
2024-03-12 23:14:35 +0200

    init commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+// ДЗ №1 - Git
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello and welcome!");
+
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("i = " + i);
+        }
+    }
+}
